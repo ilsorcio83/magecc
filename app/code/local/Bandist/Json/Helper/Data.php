e5d3862c3b17b2b15a680dcaf934934491ea7464
Giorgio De Felice <giorgiodefelice@gmail.com>
2016-08-08 15:56:57 +0200

    Core module files

---
+++ app/code/local/Bandist/Json/Helper/Data.php
@@ -0,0 +1,9 @@
+<?php
+
+/**
+ * Class Bandist_Json_Helper_Data
+ * Default Helper for the Bandist Module
+ */
+class Bandist_Json_Helper_Data extends Mage_Core_Helper_Abstract {
+        
+}
---
+++ app/code/local/Bandist/Json/controllers/IndexController.php
@@ -0,0 +1,16 @@
+<?php
+
+/**
+ * Class Bandist_Json_IndexController
+ *
+ */
+class Bandist_Json_IndexController extends Mage_Core_Controller_Front_Action {
+
+    /**
+     * Default controller action
+     */
+    public function indexAction()
+    {
+
+    }
+}
